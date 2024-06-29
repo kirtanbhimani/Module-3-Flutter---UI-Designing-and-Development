5f55c3337636fc0821b45e7eb603f68c9c39f728
Kirtan Bhimani <145966340+kirtanbhimani@users.noreply.github.com>
2024-06-29 14:09:37 +0530

    Add files via upload

---
+++ Question 50.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Question50 extends StatelessWidget {
+  const Question50({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Question 50'),
+        backgroundColor: Colors.pinkAccent,
+      ),
+      
+    );
+  }
+}
